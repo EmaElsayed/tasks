74762c3bdaf139fe15735ec06afa811233275764
EmaElsayed <emoelsayed78@gmail.com>
2023-07-12 10:55:46 +0300

    Fire base

---
+++ lib/quiz/main.dart].dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:untitled/firebase/notes_login_screen.dart';
+import 'package:untitled/firebase/notes_screen.dart';
+import 'package:untitled/quiz/quiz_admin_questions_screen.dart';
+import 'package:untitled/quiz/quiz_admin_screen.dart';
+import 'package:untitled/quiz/quiz_login_screen.dart';
+import 'package:untitled/quiz/quiz_main_screen.dart';
+import 'package:untitled/quiz/quiz_register_screen.dart';
+import 'package:untitled/quiz/start_quiz_screen.dart';
+
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+     // home: FirebaseAuth.instance.currentUser == null
+        home:QuizLoginScreen());
+        //  ? QuizLoginScreen()
+       //   : const QuizMainScreen(),
+  //  );
+  }
+
+  Widget navigateToScreen() {
+    if (FirebaseAuth.instance.currentUser == null) {
+      return NotesLoginScreen();
+    } else {
+      return NotesScreen();
+    }
+  }
+}
